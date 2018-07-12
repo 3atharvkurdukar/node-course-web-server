5a04dab8c31c570be410b524a1b3923560ea3b21
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-12 22:58:09 +0530

    Add projects folder

--- server.js
+++ server.js
@@ -61,6 +61,10 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs');
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
